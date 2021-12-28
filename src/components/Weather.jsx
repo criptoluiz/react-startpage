047746e0bc8d888db140e65e726bbdb0cd9d4bc4
Luiz <luiz.pb@cryptolab.net>
2021-12-28 04:10:02 -0300

    height

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,23 +5,6 @@ import React, { useEffect, useState } from "react";
 const apiKey = "e61aa27d22eb0b30ab346a9f6441d600";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=porto alegre&appid=${apiKey}&units=metric`;
 
-function update() {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log(data.weather[0].icon);
-
-      // Weather Icon
-      //weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-      this.icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      // Weather description
-      this.desc = data.weather[0].description;
-      console.log(this.desc);
-      // Temperature - math.round so the number isn't fractional
-      this.temp = Math.round(data.main.temp);
-    });
-}
-
 function Weather(props) {
   const [desc, setDesc] = useState([]);
   const [temp, setTemp] = useState([]);
